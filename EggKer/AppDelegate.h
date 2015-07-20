7723e38a4e4477497bfef0176c1b18a88e5a50d3
李贤军 <lixianjun0903@163.com>
2015-07-20 18:06:25 +0800

    test
    蛋壳儿测试版

---
+++ EggKer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EggKer
+//
+//  Created by 李李贤军 on 15/7/20.
+//  Copyright (c) 2015年 TH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EggKer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EggKer
+//
+//  Created by 李李贤军 on 15/7/20.
+//  Copyright (c) 2015年 TH. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
